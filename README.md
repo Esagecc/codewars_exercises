e3b956f9e1fd985a6f72d72c19a316cfdc35be75
Elizabeth Cantu <e.sage.cc@gmail.com>
2023-03-06 00:40:00 -0600

    completed the second exercise

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 I constructed this repository to note down all the exercises I complete in Code Wars site. I want to demonstrated my daily practice on the site. It is more efficient to update my Github profile my daily JavaScript work. 
+
+I was happy to complete two exercises on 03/06/2023
---
+++ two_to_one.js
@@ -0,0 +1,23 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    // your code
+    let long = s1.concat(s2);
+    let uniqueChars = [...new Set(long)];
+    let longestString = uniqueChars.sort()
+    return longestString.join('')
+    }
+
+//I had to do a lot of googling to figure this one out. I first google how combine two string and I added .concat() and then how to remove repeative characters which I used the [...new Set()]. Then I noticed the new string were in alphabetical order and I used .sort(). I first tested the code and it came back with the string seperate with a comma. I then used .join('') to combine the strings. I am proud that I did not give up and look at the solution. I know I can write the code more simpler. 
+
+const longestStr = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+//I am taking a note of this shorter code.
