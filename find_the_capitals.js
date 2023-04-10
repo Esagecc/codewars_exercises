31cd5c03b6b3be5f3468e94e20106a88b25b8cca
Elizabeth Cantu <e.sage.cc@gmail.com>
2023-04-10 01:28:48 -0500

    completed another challenge

---
+++ find_the_capitals.js
@@ -0,0 +1,13 @@
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+
+var capitals = function (word) {
+    var caps = [];
+      for(var i = 0; i < word.length; i++) {
+      if(word[i].toUpperCase() == word[i]) caps.push(i);
+    }
+    return caps;
+  };
+
+
+const capitals2 = word => { return word.match(/[A-Z]/g).map( x => { return word.indexOf(x) }) }
