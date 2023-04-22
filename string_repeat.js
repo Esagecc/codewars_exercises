fd479d79e07aee5061cf5dbe4e05021e60ed09c5
Elizabeth Cantu <e.sage.cc@gmail.com>
2023-04-22 15:26:34 -0500

    I completed a simple lesson

---
+++ string_repeat.js
@@ -0,0 +1,15 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
+
+  const repeatStrTwo = (n, s) => s.repeat(n)
+
+
+//One of the simple exercises I completed. I did not need to google a solution. It was nice to confirm some knowledge. I did also copy a shorter lines. 04/22/2023
